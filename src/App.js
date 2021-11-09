35aaff7d9c194f05d291bed035df087cd649dca7
Renan Santos <renan.061@gmail.com>
2021-11-09 17:38:25 -0300

    Muda'nca de setState para função

--- src/App.js
+++ src/App.js
@@ -3,59 +3,65 @@ import Board from "./Board.js";
 import Postit from "./Postit.js";
 
 class App extends React.Component {
-    constructor (props){
-      super (props);
-      this.state = {
-        title: "",
-        postits: [],
-      }
+    constructor (props) {
+        super (props);
+        this.state = {
+            title: "",
+            postits: [],
+        };
     }
-    handleChangeTitle = (event)=>{
-      this.setState({
-        title: event.target.value,
-      })
+
+    handleChangeTitle = (event) => {
+        this.setState({
+            title: event.target.value,
+        });
     }
-    submit = () =>{
-      let postit = <Postit 
-        name = {this.state.title}
-        items = {[]}
-        date = {new Date().toLocaleString()}
-      />
-      this.state.postits.push(postit);
-      
-      this.setState({
-        title: "",
-        postits: this.state.postits,
-      })
+
+    submit = () => {
+        this.setState(function(state) {
+            state.postits.push(<Postit 
+                name={state.title}
+                date={new Date().toLocaleString()}
+            />);
+            return {
+                title: "",
+                postits: state.postits,
+            };
+        });
     } 
 
     removePost (x) {
-      let newList = this.state.postits.filter((item, i) => i!==x)
-      this.setState( {
-        postits: newList,
-      })
+        this.setState(function(state) {
+            let newList = state.postits.filter((item, i) => i !== x);
+            return {
+                postits: newList,
+            };
+        });
     }
 
     render(){
-      return <div>
-          <div>
-            <input 
-              value= {this.state.title} 
-              type="text" placeholder="Titulo do Postit"
-              onChange= {this.handleChangeTitle}
-            />
-            <button name="Submit" onClick={this.submit} >Submit</button>
-          </div>
-          <div>
-          <ul>
-              {this.state.postits.map((postit, x)=>
-              <li key={x}>{postit}<button name="remove" onClick={()=> {this.removePost(x)}}>Remove Post</button></li> 
-              )}
-              
-          </ul>
-          </div>
+        return <div>
+            <div>
+                <input type="text" placeholder="Titulo do Postit"
+                    value={this.state.title} 
+                    onChange={this.handleChangeTitle}
+                />
+                <button name="Submit" onClick={this.submit} >Submit</button>
+            </div>
+            <div>
+                <ul>
+                    {this.state.postits.map((postit, x) =>
+                        <li key={"postit"+x}>
+                            {postit}
+                            <button name="remove" onClick={() => this.removePost(x)}>
+                                Remove Post
+                            </button>
+                        </li>
+                    )}
+                </ul>
+            </div>
         </div>
     }
-  }
+}
   
-  export default App;
+export default App;
--- src/Postit.js
+++ src/Postit.js
@@ -1,61 +1,69 @@
 import React from 'react';
 
 class Postit extends React.Component {
-    constructor (props){
-      super (props);
-      this.state = {
-        itemName: "",
-        items: [],
-        
-      }
+    constructor (props) {
+        super (props);
+        this.state = {
+            newItemName: "",
+            items: [],
+        };
     }
-    handleChangeItem = (event)=>{
-      this.setState({
-        itemName: event.target.value,
-      })
+    
+    handleChangenewItemNameName = (event) => {
+        this.setState({
+            newItemName: event.target.value,
+            items: this.state.items,
+        });
     }
-    submit = ()=>{
-      this.state.items.push({ 
-        name: this.state.itemName,
-        date: new Date().toLocaleString(),
-      });
-      this.setState({
-        items: this.state.items,
-        itemName: "",
-      })
-    }
-    removeItem = (x) =>{
-      let newList = this.state.items.filter((item, i) => i!==x)
-      this.setState( {
-        postits: newList,
-      })
-    }
-    render () {
-      return <div>
-          
-          <h1> {this.props.name}</h1>
-          <p>Criado em: {this.props.date}</p>
-          <ul>
-             {this.state.items.map((item, i)=>
-             <li key={this.props.name+i}>
-               <div>
-                 <p> {item.name}<button name="remove" onClick={()=> {this.removeItem(i)}}>-</button></p> 
-               </div>
-               <span>{item.date}</span>
-               
-             </li> 
-             )}
-            
-          </ul>
-          
-            <input 
-            value= {this.state.itemName} 
-            type="text" placeholder="Bullet do Postit"
-            onChange= {this.handleChangeItem}
-          />
-          <button name="Submit" onClick={this.submit} >Submit</button>
-        </div> 
-    }
-  }
 
-  export default Postit;
+    submit = () => {
+        this.setState(function(state) {
+            state.items.push({ 
+                name: state.newItemName,
+                date: new Date().toLocaleString(),
+            });
+            return {
+                newItemName: "",
+                items: state.items,
+            };
+        });
+    }
+
+    removeItem = (x) => {
+        this.setState(function(state) {
+            let newList = state.items.filter((item, i) => i !== x);
+            return {
+                items: newList,
+            };
+        });
+    }
+
+    render () {
+        return <div>
+            <h1>{this.props.name}</h1>
+            <p>Criado em: {this.props.date}</p>
+            <ul>
+                {this.state.items.map((item, i) =>
+                    <li key={"item"+i}>
+                        <div>
+                            <p>
+                                {item.name}
+                                <button name="remove" onClick={() => this.removeItem(i)}>
+                                    -
+                                </button>
+                            </p> 
+                        </div>
+                        <span>{item.date}</span>
+                    </li> 
+                )}
+            </ul>
+          
+            <input type="text" placeholder="Bullet do Postit"
+                value={this.state.newItemName}
+                onChange={this.handleChangenewItemNameName}/>
+            <button name="Submit" onClick={this.submit} >Submit</button>
+        </div>
+    }
+}
+
+export default Postit;
